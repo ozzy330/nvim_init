55ba40a1d7420c041fd63bc814ac9f6576e5a5fe
ozzy330 <oskar.quesada30@gmail.con>
2022-04-25 14:35:48 -0600

    Theme changed to Nordi

--- init.lua
+++ init.lua
@@ -10,59 +10,56 @@ packer = require 'packer'
 local use = packer.use
 packer.reset() -- Recommended
 
-packer.startup({function()
+packer.startup({ function()
   use 'wbthomason/packer.nvim' -- Plugint manager packer
 
-  use {'neovim/nvim-lspconfig'} -- Collection of configurations for the built-in LSP client
-  use {'williamboman/nvim-lsp-installer'} -- Insaller of LSP servers
-  use {'hrsh7th/nvim-cmp'} -- Autocompletion plugin
-  use {'hrsh7th/cmp-nvim-lsp'} -- LSP source for nvim-cmp
-  use {'hrsh7th/cmp-nvim-lua'} -- Nvim-cmp source for neovim Lua API.
-  use {'hrsh7th/cmp-buffer'} -- Nvim-cmp source for buffer words.
-  use {'hrsh7th/cmp-path'} -- Nvim-cmp source for path
-  use {'hrsh7th/cmp-cmdline'} -- Nvim-cmp source for vim's cmdline
+  use { 'neovim/nvim-lspconfig' } -- Collection of configurations for the built-in LSP client
+  use { 'williamboman/nvim-lsp-installer' } -- Insaller of LSP servers
+  use { 'hrsh7th/nvim-cmp' } -- Autocompletion plugin
+  use { 'hrsh7th/cmp-nvim-lsp' } -- LSP source for nvim-cmp
+  use { 'hrsh7th/cmp-nvim-lua' } -- Nvim-cmp source for neovim Lua API.
+  use { 'hrsh7th/cmp-buffer' } -- Nvim-cmp source for buffer words.
+  use { 'hrsh7th/cmp-path' } -- Nvim-cmp source for path
+  use { 'hrsh7th/cmp-cmdline' } -- Nvim-cmp source for vim's cmdline
 
   use 'saadparwaiz1/cmp_luasnip' -- Snippets source for nvim-cmp
   use 'L3MON4D3/LuaSnip' -- Snippets plugin
 
-  use {'sainnhe/gruvbox-material'} -- Theme
+  use { 'sainnhe/gruvbox-material' } -- Theme
+  use { 'rmehri01/onenord.nvim' }
 
   use 'nvim-lua/plenary.nvim' -- Complementary library of lua functions for some plugins
   -- Telescope
   -- Suggested / Optional dependencies
-  use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make'}
-  use {'kyazdani42/nvim-web-devicons'}
+  use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
+  use { 'kyazdani42/nvim-web-devicons' }
   use {
     'nvim-telescope/telescope.nvim',
-    requires = { {'nvim-lua/plenary.nvim'} },
-    
+    requires = { { 'nvim-lua/plenary.nvim' } },
   }
-  use { "nvim-telescope/telescope-file-browser.nvim"} -- manage files
-  use { "nvim-telescope/telescope-dap.nvim"} -- easy acces to debugger things
+  use { "nvim-telescope/telescope-file-browser.nvim" } -- manage files
+  use { "nvim-telescope/telescope-dap.nvim" } -- easy acces to debugger things
 
   -- For better syntax highlight
   use {
-      'nvim-treesitter/nvim-treesitter',
-      run = ':TSUpdate',
-      
+    'nvim-treesitter/nvim-treesitter',
+    run = ':TSUpdate',
   }
 
   -- Statusline
   use {
     'nvim-lualine/lualine.nvim',
-    requires = { 'kyazdani42/nvim-web-devicons', opt = true }, 
-    
+    requires = { 'kyazdani42/nvim-web-devicons', opt = true },
   }
 
   -- Tabs manager
   use {
     'romgrk/barbar.nvim',
-    requires = {'kyazdani42/nvim-web-devicons'},
-    
+    requires = { 'kyazdani42/nvim-web-devicons' },
   }
 
   -- PopUp Terminal
-  use {"akinsho/toggleterm.nvim"}
+  use { "akinsho/toggleterm.nvim" }
 
   -- Git integrations
   use {
@@ -73,17 +70,15 @@ packer.startup({function()
     config = function()
       require('gitsigns').setup()
     end,
-    
   }
-  use {'tpope/vim-fugitive'}
+  use { 'tpope/vim-fugitive' }
 
   -- Manage comments
   use {
     'numToStr/Comment.nvim',
     config = function()
-        require('Comment').setup()
-    end, 
-    
+      require('Comment').setup()
+    end,
   }
   use {
     "folke/todo-comments.nvim",
@@ -105,50 +100,50 @@ packer.startup({function()
           pattern = [[\b(KEYWORDS)(\([^\)]*\))?:]],
         },
       }
-    end, 
+    end,
   }
 
   -- Extra
-  use {'max397574/better-escape.nvim'}
-  use {'karb94/neoscroll.nvim'}
-  use {'andymass/vim-matchup'}
+  use { 'max397574/better-escape.nvim' }
+  use { 'karb94/neoscroll.nvim' }
+  use { 'andymass/vim-matchup' }
 
   -- Debbugger
-  use {'mfussenegger/nvim-dap'}
-  use {"Pocco81/DAPInstall.nvim"}
-  use { "rcarriga/nvim-dap-ui", requires = {"mfussenegger/nvim-dap"}}
+  use { 'mfussenegger/nvim-dap' }
+  use { "Pocco81/DAPInstall.nvim" }
+  use { "rcarriga/nvim-dap-ui", requires = { "mfussenegger/nvim-dap" } }
 
 end,
 -- Make packer use a floating window for outputs
 config = {
   display = {
     open_fn = function()
-      return require('packer.util').float({border = 'single'})
+      return require('packer.util').float({ border = 'single' })
     end
   }
-}})
+} })
 
 require('neoscroll').setup()
 require("better_escape").setup {
-    mapping = {"jk"}, -- a table with mappings to use
-    timeout = vim.o.timeoutlen, -- the time in which the keys must be hit in ms. Use option timeoutlen by default
-    clear_empty_lines = false, -- clear line after escaping if there is only whitespace
-    keys = "<Esc>", -- keys used for escaping, if it is a function will use the result everytime
-    -- example(recommended)
-    -- keys = function()
-    --   return vim.api.nvim_win_get_cursor(0)[2] > 1 and '<esc>l' or '<esc>'
-    -- end,
+  mapping = { "jk" }, -- a table with mappings to use
+  timeout = vim.o.timeoutlen, -- the time in which the keys must be hit in ms. Use option timeoutlen by default
+  clear_empty_lines = false, -- clear line after escaping if there is only whitespace
+  keys = "<Esc>", -- keys used for escaping, if it is a function will use the result everytime
+  -- example(recommended)
+  -- keys = function()
+  --   return vim.api.nvim_win_get_cursor(0)[2] > 1 and '<esc>l' or '<esc>'
+  -- end,
 }
-require'nvim-treesitter.configs'.setup {
+require 'nvim-treesitter.configs'.setup {
   matchup = {
-    enable = true,              -- mandatory, false will disable the whole extension
-    disable = { "c", "ruby" },  -- optional, list of language that will be disabled
+    enable = true, -- mandatory, false will disable the whole extension
+    disable = { "c", "ruby" }, -- optional, list of language that will be disabled
     -- [options]
   },
 }
 -- NVIM LSP
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
-local opts = { noremap=true, silent=true }
+local opts = { noremap = true, silent = true }
 vim.api.nvim_set_keymap('n', '<space>e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
 vim.api.nvim_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
 vim.api.nvim_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
@@ -174,7 +169,7 @@ local on_attach = function(client, bufnr)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>F', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>FO', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
 end
 
 -- Use a loop to conveniently call 'setup' on multiple servers and
@@ -205,15 +200,15 @@ local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protoco
 -- Register a handler that will be called for each installed server when it's ready (i.e. when installation is finished
 -- or if the server is already installed).
 lsp_installer.on_server_ready(function(server)
-    local opts = {
-      on_attach = on_attach,
-      capabilities = capabilities
-    }
-    -- NOTE: tal vez agregar aquí el compilador
-    -- This setup() function will take the provided server configuration and decorate it with the necessary properties
-    -- before passing it onwards to lspconfig.
-    -- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
-    server:setup(opts)
+  local opts = {
+    on_attach = on_attach,
+    capabilities = capabilities
+  }
+  -- NOTE: tal vez agregar aquí el compilador
+  -- This setup() function will take the provided server configuration and decorate it with the necessary properties
+  -- before passing it onwards to lspconfig.
+  -- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
+  server:setup(opts)
 end)
 
 -- Nvim-cmp && LuaSnip configuration
@@ -223,14 +218,14 @@ local has_words_before = function()
 end
 
 local luasnip = require("luasnip")
-local cmp = require'cmp'
+local cmp = require 'cmp'
 
 cmp.setup {
   snippet = {
     -- REQUIRED - you must specify a snippet engine
     expand = function(args)
-    require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-  end,
+      require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+    end,
   },
   mapping = {
     ['<C-d>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
@@ -269,11 +264,11 @@ cmp.setup {
   },
   -- Order is a ranking of priority
   sources = {
-    {name = 'nvim_lsp'},
-    {name = 'nvim_lua'},
-    {name = 'path'},
-    {name = 'buffer', keyword_length = 4}, -- Show only after 4 leters pressed
-    {name = 'cmdline'},
+    { name = 'nvim_lsp' },
+    { name = 'nvim_lua' },
+    { name = 'path' },
+    { name = 'buffer', keyword_length = 4 }, -- Show only after 4 leters pressed
+    { name = 'cmdline' },
   },
 
   experimental = {
@@ -305,40 +300,40 @@ dap.listeners.before.event_exited["dapui_config"] = function()
 end
 
 dap.adapters.lldb = {
-    type = 'executable',
-    -- absolute path is important here, otherwise the argument in the `runInTerminal` request will default to $CWD/lldb-vscode
-    command = '/usr/bin/lldb-vscode-14',
-    name = "lldb"
-  }
+  type = 'executable',
+  -- absolute path is important here, otherwise the argument in the `runInTerminal` request will default to $CWD/lldb-vscode
+  command = '/usr/bin/lldb-vscode-14',
+  name = "lldb"
+}
 dap.configurations.cpp = {
-    {
-      name = "Launch",
-      type = "lldb",
-      request = "launch",
-      program = function()
-        return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
-      end,
-      args = {},
-      cwd = '${workspaceFolder}',
-      stopOnEntry = true,
-      runInTerminal = false,
-    },
-    {
-      name = "Launch_args",
-      type = "lldb",
-      request = "launch",
-      program = function()
-        return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
-      end,
-      -- ASk for arguments
-      args = function()
-        local argument_string = vim.fn.input('Program arguments: ')
-        return vim.fn.split(argument_string, " ", true)
-      end,
-      cwd = '${workspaceFolder}',
-      stopOnEntry = true,
-      runInTerminal = false,
-    },
+  {
+    name = "Launch",
+    type = "lldb",
+    request = "launch",
+    program = function()
+      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+    end,
+    args = {},
+    cwd = '${workspaceFolder}',
+    stopOnEntry = true,
+    runInTerminal = false,
+  },
+  {
+    name = "Launch_args",
+    type = "lldb",
+    request = "launch",
+    program = function()
+      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+    end,
+    -- ASk for arguments
+    args = function()
+      local argument_string = vim.fn.input('Program arguments: ')
+      return vim.fn.split(argument_string, " ", true)
+    end,
+    cwd = '${workspaceFolder}',
+    stopOnEntry = true,
+    runInTerminal = false,
+  },
 }
 -- Used for C and C++
 dap.configurations.c = dap.configurations.cpp
@@ -363,14 +358,19 @@ require('telescope').load_extension('dap')
 require('lualine').setup()
 
 -- PopUp terminal
-require('toggleterm').setup{}
+require('toggleterm').setup {}
 
 -- Git integrations
 
 -- colors configuration
-vim.cmd("colorscheme gruvbox-material")
-require'nvim-treesitter.configs'.setup {
-  highlight = {enable = true },
+vim.cmd [[colorscheme onenord]]
+require 'nvim-treesitter.configs'.setup {
+  highlight = { enable = true },
+}
+require('lualine').setup {
+  options = {
+    theme = 'onenord'
+  }
 }
 
 -- inactive statuslines as thin lines
@@ -413,23 +413,23 @@ vim.g.mapleader = " "
 
 -- easy keymapping
 local keymap = vim.api.nvim_set_keymap
-local options = {noremap = true, silent = true}
+local options = { noremap = true, silent = true }
 
-keymap('i', 'jk', '<esc>', {noremap = true}) -- exit insert mode
-keymap('i', '<c-h>', '<c-w>', {noremap = true}) -- delete previous word (h = bs)
+keymap('i', 'jk', '<esc>', { noremap = true }) -- exit insert mode
+keymap('i', '<c-h>', '<c-w>', { noremap = true }) -- delete previous word (h = bs)
 
 -- manage splits
 keymap('n', '<C-s>l', ':vsp<cr>', options) -- create vertical split
 keymap('n', '<C-s>j', ':sp<cr>', options) -- create horizontal split
-keymap('n', '-', '5<c-w>+', {noremap = true}) -- increase split height
-keymap('n', '+', '5<c-w>>', {noremap = true}) -- increase split width
-keymap('n', '=','<c-w>=', {noremap = true}) -- normalize all split sizes
+keymap('n', '-', '5<c-w>+', { noremap = true }) -- increase split height
+keymap('n', '+', '5<c-w>>', { noremap = true }) -- increase split width
+keymap('n', '=', '<c-w>=', { noremap = true }) -- normalize all split sizes
 keymap('n', '<C-s>x', ':only<cr>', options) -- close all but current split
 
-keymap('n', '<c-j>', '<c-w><c-j>', {noremap = true}) -- move to split below
-keymap('n', '<c-k>', '<c-w><c-k>', {noremap = true}) -- move to split above
-keymap('n', '<c-l>', '<c-w><c-l>', {noremap = true}) -- move to split on the right
-keymap('n', '<c-h>', '<c-w><c-h>', {noremap = true}) -- move to split on the left
+keymap('n', '<c-j>', '<c-w><c-j>', { noremap = true }) -- move to split below
+keymap('n', '<c-k>', '<c-w><c-k>', { noremap = true }) -- move to split above
+keymap('n', '<c-l>', '<c-w><c-l>', { noremap = true }) -- move to split on the right
+keymap('n', '<c-h>', '<c-w><c-h>', { noremap = true }) -- move to split on the left
 
 -- manage buffers
 keymap('n', '<s-l>', ':BufferNext<CR>', options) -- go to next buffer
